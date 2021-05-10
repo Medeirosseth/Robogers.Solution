cf2dbaa6f2a02f354de67338cf02563b4bf6a955
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-10 13:48:25 -0700

    add .mvcfiles
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

---
+++ MrRoboger/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code
+
+function beepboop(slang) {
+  let robotSlang = [];
+  for (let index = 0; index <= slang; index++) {
+    if (index.toString().includes("3")) {
+      robotSlang.push("Wont you be my neighbor?");
+    } else if (index.toString().includes("2")) {
+      robotSlang.push("BOOP");
+    } else if (index.toString().includes("1")) {
+      robotSlang.push("BEEP!");
+    } else {
+      robotSlang.push(index.toString());
+    }
+  }
+  return robotSlang;
+}
+
+//User Interface
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    event.preventDefault();
+    let robotSlang = $("input#translator").val();
+    let robotLanguage = beepboop(robotSlang);
+    $("#results").text(robotLanguage.toString());
+  });
+});
